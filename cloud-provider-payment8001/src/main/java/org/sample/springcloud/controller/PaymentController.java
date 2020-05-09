6b031037add5e1f1c6d5ad262bcfff40f8cd7b7b
yyaat <yyaat@foxmail.com>
2020-05-09 21:25:30 +0800

    eureka集群配置  客户端配置 负载均衡 及eureka界面显示ip和自定义服务名 instance-id C:\Windows\System32\drivers\etc\hosts   添加以下两行   127.0.0.1 eureka7001.com   127.0.0.1 eureka7002.com
    测试访问路径
    http://eureka7001.com:7001/
    http://eureka7002.com:7002/
    http://localhost/consumer/payment/get/31

--- cloud-provider-payment8001/src/main/java/org/sample/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/org/sample/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.sample.springcloud.entities.CommonResult;
 import org.sample.springcloud.entities.Payment;
 import org.sample.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,15 +16,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value="/payment/save")
     public CommonResult save(@RequestBody Payment payment) {
 
         int result = paymentService.save(payment);
         log.info("*****插入结果{}",result);
         if(result > 0) {
-            return new CommonResult(200,"插入成功",result);
+            return new CommonResult(200,String.format("插入成功,端口号：%s",serverPort),result);
         }else{
-            return new CommonResult(444,"插入失败");
+            return new CommonResult(444,String.format("插入失败,端口号：%s",serverPort));
         }
     }
 
@@ -33,9 +37,9 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****结果{}",payment);
         if(payment != null) {
-            return new CommonResult(200,"成功",payment);
+            return new CommonResult(200,String.format("查询成功,端口号：%s",serverPort),payment);
         }else{
-            return new CommonResult(444,String.format("没有对应记录，%s", id));
+            return new CommonResult(444,String.format("没有对应记录，id:%s 端口号:%s", id , serverPort));
         }
     }
 }
